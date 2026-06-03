89068759dfe02668ac91a89458f1b9415f1a656a
Truong <buidangthientruong@gmail.com>
2026-06-03 16:30:20 +0700

    x

--- javascript/login.js
+++ javascript/login.js
@@ -1,39 +1,24 @@
-document
-.getElementById("loginForm")
-.addEventListener("submit", function(e){
+document.getElementById("loginForm").addEventListener("submit", function (e) {
+  e.preventDefault();
 
-    e.preventDefault();
+  const username = document.getElementById("username").value;
 
-    const username =
-        document.getElementById("username").value;
+  const password = document.getElementById("password").value;
 
-    const password =
-        document.getElementById("password").value;
+  const user = JSON.parse(localStorage.getItem("user"));
 
-    const user =
-        JSON.parse(localStorage.getItem("user"));
+  if (!user) {
+    alert("Chưa có tài khoản");
+    return;
+  }
 
-    if(!user){
-        alert("Chưa có tài khoản");
-        return;
-    }
+  if (username === user.username && password === user.password) {
+    alert("Đăng nhập thành công");
 
-    if(
-        username === user.username &&
-        password === user.password
-    ){
-        alert("Đăng nhập thành công");
+    localStorage.setItem("isLogin", "true");
 
-        localStorage.setItem(
-            "isLogin",
-            "true"
-        );
-
-        window.location.href = "../index.html";
-    }
-    else{
-        alert("Sai tài khoản hoặc mật khẩu");
-    }
-
+    window.location.href = "../index.html";
+  } else {
+    alert("Sai tài khoản hoặc mật khẩu");
+  }
 });
-
